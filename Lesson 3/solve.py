a04948f9705fa05fd7ddcc62834f5efbc68dab11
Yuri Corredor <YuriCorredor159@gmail.com>
2022-05-05 15:43:24 -0300

    Lesson 3

---
+++ Lesson 3/solve.py
@@ -0,0 +1,12 @@
+from tests import tests
+from evaluate_tests import evaluate_tests
+
+def brute_force():
+    pass
+
+evaluate_tests(brute_force, tests)
+
+def solve():
+    pass
+
+evaluate_tests(solve, tests)
---
+++ Lesson 3/tests.py
@@ -0,0 +1,8 @@
+tests = []
+
+tests.append({
+    'input': {
+        'nums': [19, 25, 29, 3, 5, 6, 7, 9, 11, 14]
+    },
+    'output': 3
+})
